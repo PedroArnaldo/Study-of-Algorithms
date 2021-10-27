bb0b359e42c477b7b528f4dcdce0945ac963caca
PedroArnaldo <pedromadeira201502@outlook.com>
2021-10-27 11:28:27 -0300

    add ex-4-b_cap3

---
+++ Capitulo 4/4-B/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _4_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de temperatura de Fahrenheit para Celsius");
+
+            float temperaturaCelsius;
+            float temperaturaFahrenheit;
+
+            temperaturaFahrenheit = float.Parse(Console.ReadLine());
+
+            temperaturaCelsius = ((temperaturaFahrenheit - 32) * 5)/9;
+
+
+            Console.WriteLine(temperaturaCelsius);
+        }
+    }
+}
