231c7250f91ef6690b3a564ab5ea4a2a5dfe5129
Pedro Lopes <pedromadeira201502@outlook.com>
2023-12-05 08:31:48 -0300

    add: python exercise 3

---
+++ python_exercise/exercise3.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores']
+
+for item in lista_produtos:
+    print("Temos %s à venda!" % item)
