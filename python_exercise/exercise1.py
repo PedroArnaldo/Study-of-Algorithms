0c955971d91f8f91ef911f141280d89da7496b57
Pedro Lopes <pedromadeira201502@outlook.com>
2023-12-05 07:47:13 -0300

    add: python exercise 1

---
+++ python_exercise/exercise1.py
@@ -0,0 +1,24 @@
+list_product = ['teclado', 'mouse', 'monitor', 'gabinete', 'placa de vídeo', 'processador']
+
+
+print("\n\nPrint list product computer shop:")
+for item in list_product:
+    print(item)
+
+
+print("\n\n\n")
+
+
+list_family = {
+    'Maria': 1988,
+    'Luiza': 1976,
+    'José': 1960,
+    'Márcia': 1969,
+    'Rafaela': 1996
+}
+
+print("Print list family:")
+for key, value in list_family.items():
+    print(f"{key}: {value}")
+
+
