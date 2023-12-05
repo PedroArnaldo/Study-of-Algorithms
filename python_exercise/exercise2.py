383c0291bf690a63a64d413912a92bde7ec0998f
Pedro Lopes <pedromadeira201502@outlook.com>
2023-12-05 08:23:27 -0300

    add: python exercise 2

---
+++ python_exercise/exercise2.py
@@ -0,0 +1,7 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+print("Tamanho da lista de musicos: %s \n" % len(lista_musicos))
+
+print("dados do índice 2: %s\n" % lista_musicos[2])
+print("dados do índice 9: %s\n" % lista_musicos[9])
+print("dados do índice 14: %s" % lista_musicos[14])
