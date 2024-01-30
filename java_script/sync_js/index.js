58b2f9a3997126698a03b5352d253e1ab016ca4c
Pedro Lopes <pedromadeira201502@outlook.com>
2024-01-30 11:39:26 -0300

    feat: use promise

--- java_script/sync_js/index.js
+++ java_script/sync_js/index.js
@@ -5,23 +5,32 @@
 
 */
 
-const getUser = (callback) => {
-  setTimeout(() => {
-    return callback(null, {
-      id: 1,
-      nome: "Aladin",
-      dataNascimento: new Date(),
-    });
-  }, 1000);
+const util = require("util");
+
+const getUser = () => {
+  //quando der algum problema -> reject(Error)
+  //quando sucess -> resolve
+  return new Promise(function solvePromise(resolve, reject) {
+    setTimeout(() => {
+      return resolve({
+        id: 1,
+        nome: "Aladin",
+        dataNascimento: new Date(),
+      });
+    }, 1000);
+  });
 };
 
-const getPhone = (idUser, callback) => {
-  setTimeout(() => {
-    return callback(null, {
-      telefone: "983123121",
-      ddd: "12",
-    });
-  }, 1000);
+const getPhone = (idUser) => {
+  return new Promise((resolve, reject) => {
+    if (!idUser) return reject();
+    setTimeout(() => {
+      return resolve({
+        telefone: "983123121",
+        ddd: "12",
+      });
+    }, 1000);
+  });
 };
 
 const getAddress = (idUser, callback) => {
@@ -29,30 +38,58 @@ const getAddress = (idUser, callback) => {
     return callback(null, {
       rua: "dos bobos",
     });
-  }, 2000);
+  });
 };
 
-const user = getUser((erro, user) => {
-  if (erro) {
-    console.error("Error in user", erro);
-  }
-  getPhone(user.id, (erro1, phone) => {
-    if (erro1) {
-      console.error("Error in phone", erro1);
-    }
-    console.log("phone: ", phone);
-    return phone;
-  });
-  getAddress(user.id, (erro2, address) => {
-    if (erro2) {
-      console.error("Error in address", erro2);
-    }
-    console.log("address: ", address);
-    return address;
-  });
-  console.log("user: ", user);
-  return user;
-});
+const getAddressAsync = util.promisify(getAddress);
+//no then usa o conceito de pipe
+// const userPromise = getUser();
+// userPromise
+//   .then((user) => {
+//     return getPhone(user.id).then((phone) => {
+//       return {
+//         user,
+//         phone,
+//       };
+//     });
+//   })
+//   .then((userWithPhone) => {
+//     return getAddressAsync(userWithPhone.user.id).then((address) => {
+//       return {
+//         data: userWithPhone.user,
+//         phone: userWithPhone.phone,
+//         address: address,
+//       };
+//     });
+//   })
+//   .then((user) => {
+//     console.log("user: ", user);
+//   })
+//   .catch((error) => {
+//     console.error("Error :", error);
+//   });
+
+// const user = getUser((erro, user) => {
+//   if (erro) {
+//     console.error("Error in user", erro);
+//   }
+//   getPhone(user.id, (erro1, phone) => {
+//     if (erro1) {
+//       console.error("Error in phone", erro1);
+//     }
+//     console.log("phone: ", phone);
+//     return phone;
+//   });
+//   getAddress(user.id, (erro2, address) => {
+//     if (erro2) {
+//       console.error("Error in address", erro2);
+//     }
+//     console.log("address: ", address);
+//     return address;
+//   });
+//   console.log("user: ", user);
+//   return user;
+// });
 // const phone = getPhone(user.id);
 // const address = getAddress(user.id);
 
